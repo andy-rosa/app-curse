535956ddf22eb97621165f4bcd4b05c23c8fd5b2
Andy Rosa <andy.roshitu@gmail.com>
2023-02-24 01:18:04 +0300

    add eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { type ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home (): ReactNode {
   return (
     <>
       <Head>
